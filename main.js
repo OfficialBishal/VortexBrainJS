f8e76b2b55cf575cec5a864550f4af3ad7a92010
Bishal Shrestha <bishal.modern@gmail.com>
2023-02-17 22:46:29 +0545

    commit

--- main.js
+++ main.js
@@ -1,51 +1,69 @@
-import http from "https://unpkg.com/isomorphic-git@beta/http/web/index.js";
+// ------------ Code of ckeditor ------------
+// Get a reference to the editor element
+var editorElement = [];
+
+const editorElement0 = document.querySelector("#question0");
+const editorElement1 = document.querySelector("#question1");
+const editorElement2 = document.querySelector("#question2");
+
+// Set initial data for the editor
+editorElement0.innerHTML = "<p>Hello from CKEditor 5!</p>";
+editorElement1.innerHTML = "<p>Hello from CKEditor 5!</p>";
+editorElement2.innerHTML = "<p>Hello from CKEditor 5!</p>";
+
+editorElement.push(editorElement0);
+editorElement.push(editorElement1);
+editorElement.push(editorElement2);
+
+var data = [];
+
+// Create a new instance of ClassicEditor
+editorElement.map((element, i) => {
+  ClassicEditor.create(element)
+    .then((editor) => {
+      console.log("Editor is ready to use!", editor);
+
+      // Add onChange event listener
+      editor.model.document.on("change:data", () => {
+        data[i] = editor.getData();
+        console.log("Data:", data[i]);
+      });
+
+      // Add onBlur event listener
+      editor.editing.view.document.on("blur", () => {
+        console.log("Blur.", editor);
+      });
+
+      // Add onFocus event listener
+      editor.editing.view.document.on("focus", () => {
+        console.log("Focus.", editor);
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+// ------------ Isomorphic-git ------------
 
 // Initialize isomorphic-git with a file system
 window.fs = new LightningFS("fs");
 // I prefer using the Promisified version honestly
 window.pfs = window.fs.promises;
 
-// ------------ Code of ckeditor ------------
-// Get a reference to the editor element
-const editorElement = document.querySelector("#editor");
-
-// Set initial data for the editor
-editorElement.innerHTML = "<p>Hello from CKEditor 5!</p>";
-var data = "";
-// Create a new instance of ClassicEditor
-ClassicEditor.create(editorElement)
-  .then((editor) => {
-    console.log("Editor is ready to use!", editor);
-
-    // Add onChange event listener
-    editor.model.document.on("change:data", () => {
-      data = editor.getData();
-      console.log("Data:", data);
-    });
-
-    // Add onBlur event listener
-    editor.editing.view.document.on("blur", () => {
-      console.log("Blur.", editor);
-    });
-
-    // Add onFocus event listener
-    editor.editing.view.document.on("focus", () => {
-      console.log("Focus.", editor);
-    });
-  })
-  .catch((error) => {
-    console.error(error);
-  });
-
-// ------------ Start writing from here ------------
 console.log("Hello");
 
+var allData = "";
+
 async function testSecond() {
-  console.log(data);
+  data.map((data, i) => {
+    allData = allData + data + "$$$$$";
+  });
+  console.log(allData);
   await git.init({ fs, dir: "/tigers" });
   console.log("init done");
 
-  await fs.promises.writeFile("/tigers/README.md", data);
+  await fs.promises.writeFile("/tigers/README.md", allData);
   await git.add({ fs, dir: "/tigers", filepath: "README.md" });
   console.log("file adding done");
 
@@ -73,7 +91,7 @@ async function testSecond() {
     });
 }
 
-let btnTest = document.getElementById("btnTest");
+let btnTest = document.getElementById("next");
 btnTest.addEventListener("click", (event) => {
   testSecond();
 });
